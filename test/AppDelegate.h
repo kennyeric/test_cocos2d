8c3026a8766bbfae959722d4d29ce2df4ddd99d6
kennyeric <kenny.xu.chuan@gmail.com>
2013-05-30 14:04:08 +0800

    Initial Commit

---
+++ test/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  test
+//
+//  Created by kenny on 5/30/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
